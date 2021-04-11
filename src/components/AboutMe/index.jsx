501b5ba9b8bbddd15687d646975203e02b2be79c
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-11 04:49:41 -0400

    saving changes to about me

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 function AboutMe() {
   return (
-    <section id="about">
+    <div id="about">
       <div className="row">
         <div className="three columns">
-            <img className="profile-pic" src="./assets/images/bio-image.jpg" alt="Profile Pic" />
+          <img
+            className="profile-pic"
+            src="./assets/images/bio-image.jpg"
+            alt="Profile Pic"
+          />
         </div>
         <div className="nine columns main-col">
           <h2>About Me</h2>
@@ -21,24 +25,28 @@ function AboutMe() {
             <div className="cloumns contact-details">
               <h2>Contact Details</h2>
               <p className="address">
-                <span>Dhruvi Vaghani</span><br />
-                <span>Street</span><br />
-                <span>City, State Zip</span><br />
-                <span>Phone Number</span><br />
-                <span>dhruvi.vaghani@yahoo.com</span><br />
+                <span>Dhruvi Vaghani</span>
+                <br />
+                <span>Street</span>
+                <br />
+                <span>City, State Zip</span>
+                <br />
+                <span>Phone Number</span>
+                <br />
+                <span>dhruvi.vaghani@yahoo.com</span>
+                <br />
               </p>
             </div>
             <div className="columns download">
               <p>
-              <iframe src="https://drive.google.com/file/d/1kWhdt8eeZWOjcAzEYwrCZbLfWHKZAt_S/preview" />
+                <iframe src="https://drive.google.com/file/d/1kWhdt8eeZWOjcAzEYwrCZbLfWHKZAt_S/preview" />
               </p>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
-
 
 export default AboutMe;
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,38 +1,51 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-function Contact() {
-    const [formState, setFormState] = useState({ name: "", email: "", message: "" });
-    const { name, email, message } = formState;
+class Contact extends Component {
+    render() {
+        return (
+            <section id="contact">
+                <div className="row section-head">
+                    <div className="two columns header-col">
+                        <h1><span>Contact Form</span></h1>
+                    </div>
 
-    function handleSubmit(e) {
-        e.preventDefault();
+                    <div className="rows">
+                        <div className="eight columns">
+                            <form action="" method="post" id="contact-form" name="contact-form">
+                        <fieldset>
+
+
+                        <div >
+                            <label htmlFor="name">Name: </label>
+                            <input type="text" name="name" defaultValue="" size="35" id="contactName" onChange={this.handleChange}/>
+                         </div>
+                        <div>
+                            <label htmlFor="email">Email: </label>
+                            <input type="email" name="email" defaultValue="" size="35" id="contactEmail" onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="message">Message: </label>
+                            <input id="contactMessage" cols="50" rows="15" name="message" />
+                        </div>
+                            <div>
+                                <button class="submit">
+                                 Submit
+                                </button>
+                                    <span id="image-loader">
+                                     <img alt="" src="loader.gif" />
+                                    </span>
+                            </div>
+                            </fieldset>
+                            </form>
+                    </div>
+                    <div id="message-alert">
+                        <i className="fa fa-check"></i>Message sent successfully!<br />
+                    </div>
+                </div>
+            </section>
+        );
     }
-
-    return (
-        <section class="justify-content-center" id="contact-form">
-            <h3 className="contact" data-testid="h2tag">Contact Form</h3>
-            <br></br>
-            <form class="justify-content-center" id="contact">
-                <div class="mt-5">
-                    <label htmlFor="name">Name: </label>
-                    <input class="form-control" type="text" name="name" defaultValue={name} />
-                </div>
-                <div class="mt-5">
-                    <label htmlFor="email">Email: </label>
-                    <input class="form-control" type="email" name="email" defaultValue={email} />
-                </div>
-                <div class="mt-5">
-                    <label htmlFor="name">Message: </label>
-                    <input class="form-control" type="text" name="message" defaultValue={message} />
-                </div>
-                <div class="mt-5 mb-5">
-                    <button class="btn btn-outline-dark mt-4" type="submit" data-testid="button" onSubmit={handleSubmit}>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </section>
-    );
 };
+
 
 export default Contact;
