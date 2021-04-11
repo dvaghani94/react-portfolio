5ee6e34a386d60467f15614b16a11829576d766d
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-11 03:53:12 -0400

    adding REsume

---
+++ src/components/Resume/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="mb-5">
+      <h3 className="resume">Resume</h3>
+      <br></br>
+
+      <div class="row justify-content-center" id="resume">
+        <div class="mt-5 pl-5 pr-5">
+          <h3>Dhruvi Vaghani</h3>
+          <div class="mt-5">
+            <iframe
+              src="https://drive.google.com/file/d/1kWhdt8eeZWOjcAzEYwrCZbLfWHKZAt_S/preview"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
+
