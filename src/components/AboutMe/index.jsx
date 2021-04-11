96c30a9f1469930b29df5884a84efc66f87181c7
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-11 04:24:12 -0400

    changing things up

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,19 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
 function AboutMe() {
   return (
-    <nav className="header">
-      <h1 class="name">Dhruvi Vaghani</h1>
-      <br></br>
-
-      <div className="row justify-content-center">
-        <div className="col-10" id="about-section">
-          <img
-            class="mb-5"
-            id="bckground"
-            src="./assets/images/bio-image.jpg"
-          />
-
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+            <img className="profile-pic" src="./assets/images/bio-image.jpg" alt="Profile Pic" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
           <p>
             My name is Dhruvi Vaghani. I was born in Belleville, New Jersey and
             lived most of my life in Edison, New Jersey. My family is a
@@ -22,10 +17,28 @@ function AboutMe() {
             bachelor's degree in Pyschology and Human Resource Management.
             Currently, I am working as a lab assistant at a cosmetics company.
           </p>
+          <div className="row">
+            <div className="cloumns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>Dhruvi Vaghani</span><br />
+                <span>Street</span><br />
+                <span>City, State Zip</span><br />
+                <span>Phone Number</span><br />
+                <span>dhruvi.vaghani@yahoo.com</span><br />
+              </p>
+            </div>
+            <div className="columns download">
+              <p>
+              <iframe src="https://drive.google.com/file/d/1kWhdt8eeZWOjcAzEYwrCZbLfWHKZAt_S/preview" />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
+    </section>
   );
 }
+
 
 export default AboutMe;
