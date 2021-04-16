5979300ef4fb4926ac303353c04b22cab7750d7b
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-15 22:10:26 -0400

    editing app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Header from "./components/Header"
+import React from "react";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
