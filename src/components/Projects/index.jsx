7774e9cf1860d6d2f10959c06416b270d31e22ea
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-15 22:43:48 -0400

    updating projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Works from "../../components/Works"
 import "bootstrap/dist/css/bootstrap.min.css";
 import projects from "../../projects.json";
 
@@ -6,22 +7,6 @@ function Wrapper(props) {
     return (
     <div className="wrapper">{props.children}</div>
     )
-}
-
-function Project(props) {
-    return (
-        <div className="card">
-            <div className="img-container">
-                <img src={props.image} alt={props.name} />
-            </div>
-            <div className="content">
-                <p className="project-title" id="card-title">{props.name}</p>
-                <p id="project-icons-container">
-                    <a href={props.github}><img src="https://img.icons8.com/fluent/48/000000/github.png" id="project-icon" alt="Github" /></a>
-                </p>
-            </div>
-        </div>
-    );
 }
 
 function Projects () {
@@ -33,7 +18,7 @@ function Projects () {
             </div>
             <Wrapper id="card-data">
                 {projects.map((project) => (
-                    <Project key={project.id} name={project.name} image={project.image} deploy={project.deploy} github={project.github} />
+                    <Works key={project.id} name={project.name} image={project.image} deploy={project.deploy} github={project.github} />
                     ))}
             </Wrapper>
         </section>
